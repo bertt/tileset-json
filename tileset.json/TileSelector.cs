3f63007b2b71948673dcb175fc0174d4bdab8da7
bert <btemme@hotmail.com>
2019-01-16 10:57:17 +0100

    add orientation

--- tileset.json/TileSelector.cs
+++ tileset.json/TileSelector.cs
@@ -5,7 +5,7 @@ namespace Tileset.Json
 {
     public static class TileSelector
     {
-        public static List<Child> GetTilesInView(Vector3 cameraPosition)
+        public static List<Child> GetTilesInView(Vector3 cameraPosition, Vector3 cameraOrientation)
         {
             // todo: make a selection of the tiles based on camera point
             return null;
