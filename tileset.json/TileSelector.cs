7c38afebff4954b618e53f9c29a46c572c15748b
bert <btemme@hotmail.com>
2019-01-16 11:52:39 +0100

    add distanceLod parameter to GetTilesInView

--- tileset.json/TileSelector.cs
+++ tileset.json/TileSelector.cs
@@ -5,9 +5,12 @@ namespace Tileset.Json
 {
     public static class TileSelector
     {
-        public static List<Child> GetTilesInView(Vector3 cameraPosition, Vector3 cameraOrientation)
+        public static List<Child> GetTilesInView(Vector3 cameraPosition, Vector3 cameraOrientation, double[] distanceLod)
         {
-            // todo: make a selection of the tiles based on camera point
+            // todo: make a selection of the tiles based on camera point and orientation
+
+            // also need to have a distance array for switchting between lod's
+
             return null;
         }
     }
