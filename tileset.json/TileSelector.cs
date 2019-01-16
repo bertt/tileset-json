b74ba72023078b3ecf7e6cf3bde6f5d9e3ac5740
bert <btemme@hotmail.com>
2019-01-16 10:34:34 +0100

    add tileselector

---
+++ tileset.json/TileSelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Tileset.Json
+{
+    public static class TileSelector
+    {
+        public static List<Child> GetTilesInView()
+        {
+            // todo: make a selection of the tiles based on camera point
+            return null;
+        }
+    }
+}
