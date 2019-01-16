cc94d63d05dc4262cdf3bde75992b336bb64d025
bert <btemme@hotmail.com>
2019-01-16 10:52:30 +0100

    change tileselector

--- tileset.json/TileSelector.cs
+++ tileset.json/TileSelector.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Numerics;
 
 namespace Tileset.Json
 {
     public static class TileSelector
     {
-        public static List<Child> GetTilesInView()
+        public static List<Child> GetTilesInView(Vector3 cameraPosition)
         {
             // todo: make a selection of the tiles based on camera point
             return null;
